e90cfe74d021b865e54f48d14e80651bd9c6a8c2
Jonas Toth <development@jonas-toth.eu>
2019-08-27 13:37:02 +0200

    Squashed 'cpp-taskflow/' content from commit 24072c59f4
    git-subtree-dir: cpp-taskflow
    git-subtree-split: 24072c59f4e2c917a62beb61397aa9c59251dcee

---
+++ example/parallel_for.cpp
@@ -0,0 +1,38 @@
+#include <taskflow/taskflow.hpp>
+#include <cassert>
+#include <numeric>
+
+// Procedure: parallel_for_on_range
+void parallel_for_on_range(int N) {
+
+  std::vector<int> range(N);
+  std::iota(range.begin(), range.end(), 0);
+
+  tf::Taskflow tf;
+  tf.parallel_for(range.begin(), range.end(), [&] (const int i) { 
+    printf("parallel_for on container item: %d\n", i);
+  });
+  tf::Executor().run(tf);
+}
+
+// Procedure: parallel_for_on_index
+void parallel_for_on_index(int N) {
+  tf::Taskflow tf;
+
+  // [0, N) with step size 1
+  tf.parallel_for(0, N, 1, [] (int i) {
+    printf("parallel_for on index: %d\n", i);
+  });
+  tf::Executor().run(tf);
+}
+
+// ----------------------------------------------------------------------------
+
+// Function: main
+int main(int argc, char* argv[]) {
+
+  parallel_for_on_range(10);
+  parallel_for_on_index(10);
+
+  return 0;
+}
---
+++ example/simple.cpp
@@ -0,0 +1,30 @@
+// A simple example to capture the following task dependencies.
+//
+// TaskA---->TaskB---->TaskD
+// TaskA---->TaskC---->TaskD
+
+#include <taskflow/taskflow.hpp>  // the only include you need
+
+int main(){
+
+  tf::Taskflow taskflow;
+  
+  auto [A, B, C, D] = taskflow.emplace( 
+    [] () { std::cout << "TaskA\n"; },     //                                 
+    [] () { std::cout << "TaskB\n"; },     //          +---+                  
+    [] () { std::cout << "TaskC\n"; },     //    +---->| B |-----+            
+    [] () { std::cout << "TaskD\n"; }      //    |     +---+     |            
+  );                                       //  +---+           +-v-+          
+                                           //  | A |           | D |          
+  A.precede(B);  // B runs after A         //  +---+           +-^-+          
+  A.precede(C);  // C runs after A         //    |     +---+     |            
+  B.precede(D);  // D runs after B         //    +---->| C |-----+            
+  C.precede(D);  // D runs after C         //          +---+
+
+  tf::Executor().run(taskflow);  // create an executor to run the taskflow
+
+  return 0;
+}
+
+
+
