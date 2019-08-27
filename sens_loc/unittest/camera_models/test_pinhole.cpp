06cc660c07a418e2f28839b4ea04af02e185d21f
Jonas Toth <development@jonas-toth.eu>
2019-08-27 15:18:23 +0200

    [Test] add unit tests for field-of-view in degree

--- sens_loc/unittest/camera_models/test_pinhole.cpp
+++ sens_loc/unittest/camera_models/test_pinhole.cpp
@@ -17,5 +17,12 @@ TEST_CASE("Calculate angular resolution") {
         REQUIRE(p.x_resolution() == doctest::Approx(0.0015094548));
         REQUIRE(p.y_resolution() == doctest::Approx(0.0018963041));
         REQUIRE(p.x_resolution() < p.y_resolution());
+
+        // field-of-view in degree for better understandability.
+        constexpr double pi = 3.141'592'653'589'793'238'462;
+        const double deg_x = (p.x_resolution() * p.w) * (180. / pi);
+        REQUIRE(deg_x == doctest::Approx(83.0259736757));
+        const double deg_y = (p.y_resolution() * p.h) * (180. / pi);
+        REQUIRE(deg_y == doctest::Approx(58.6711201045));
     }
 }
