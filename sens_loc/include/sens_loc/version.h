625df628ff0a7e7f555d881275e3140d2813fb69
Jonas Toth <development@jonas-toth.eu>
2019-08-27 12:21:14 +0200

    [Misc] create version file

---
+++ sens_loc/include/sens_loc/version.h
@@ -0,0 +1,22 @@
+#ifndef PROJECT_VERSION_H_IN_SXCFIRSV
+#define PROJECT_VERSION_H_IN_SXCFIRSV
+
+#include <string>
+
+namespace sens_loc {
+
+#define SENS_LOC_MAJOR 0
+#define SENS_LOC_MINOR 1
+#define SENS_LOC_PATCH 0
+
+inline std::string get_version() {
+    return std::to_string(SENS_LOC_MAJOR) + "." +
+           std::to_string(SENS_LOC_MINOR) + "." +
+           std::to_string(SENS_LOC_PATCH);
+}
+inline int get_major_version() { return SENS_LOC_MAJOR; }
+inline int get_minor_version() { return SENS_LOC_MINOR; }
+inline int get_patch_version() { return SENS_LOC_PATCH; }
+}  // namespace sens_loc
+
+#endif /* end of include guard: PROJECT_VERSION_H_IN_SXCFIRSV */
---
+++ sens_loc/unittest/test_version.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <algorithm>
+#include <doctest/doctest.h>
+#include <sens_loc/version.h>
+
+using namespace sens_loc;
+
+TEST_CASE("Create Versionstring") {
+    std::string version = get_version();
+
+    // <number>.<number>.<number>
+    REQUIRE(version.size() >= 5);
+    REQUIRE(std::count_if(std::begin(version), std::end(version),
+                          [](char c) { return c == '.'; }) == 2);
+}
+
+TEST_CASE("Get Version numbers") {
+    REQUIRE(get_major_version() >= 0);
+    REQUIRE(get_minor_version() >= 0);
+    REQUIRE(get_patch_version() >= 0);
+}
