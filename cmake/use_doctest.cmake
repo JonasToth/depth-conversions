24cd491cfa5975917845bea6d5b63ea5d287344f
Jonas Toth <development@jonas-toth.eu>
2019-09-03 19:30:28 +0200

    [Refactor] move doctest into third_party/

--- cmake/use_doctest.cmake
+++ cmake/use_doctest.cmake
@@ -3,7 +3,7 @@ set(DOCTEST_WITH_MAIN_IN_STATIC_LIB ON
     CACHE BOOL "DOCTEST static lib overwritten")
 set(DOCTEST_WITH_TESTS OFF CACHE BOOL "DOCTEST testing overwritten")
 
-add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/doctest")
+add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/doctest")
 mark_as_advanced(FORCE DOCTEST_NO_INSTALL 
                        DOCTEST_WITH_MAIN_IN_STATIC_LIB 
                        DOCTEST_WITH_TESTS)
