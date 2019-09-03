b18570a9c7ca1e01492e46eb7cf4dc81bac7604e
Jonas Toth <development@jonas-toth.eu>
2019-09-03 19:29:40 +0200

    [Refactor] move cpp-taskflow into third_party/

--- cmake/use_taskflow.cmake
+++ cmake/use_taskflow.cmake
@@ -1,7 +1,7 @@
 set(TF_BUILD_BENCHMARKS OFF CACHE BOOL "Taskflow benchmarks overwritten")
 set(TF_BUILD_EXAMPLES OFF CACHE BOOL "Taskflow examples overwritten")
 set(TF_BUILD_TESTS OFF CACHE BOOL "Taskflow tests overwritten")
-add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/cpp-taskflow")
+add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/cpp-taskflow")
 mark_as_advanced(FORCE TF_BUILD_BENCHMARKS 
                        TF_BUILD_EXAMPLES
                        TF_BUILD_TESTS)
