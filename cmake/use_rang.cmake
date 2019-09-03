45b42180272d783741bdd6ef87860ceca66c12f1
Jonas Toth <development@jonas-toth.eu>
2019-09-03 20:13:17 +0200

    [Refactor] move rang into third_party as well

--- cmake/use_rang.cmake
+++ cmake/use_rang.cmake
@@ -1,2 +1,3 @@
 add_library(rang INTERFACE)
-target_include_directories(rang INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/rang/include/")
+target_include_directories(rang 
+    INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/third_party/rang/include/")
