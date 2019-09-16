b100b8c306303d543e584322931ce805ef8c10a0
Jonas Toth <development@jonas-toth.eu>
2019-09-16 19:48:52 +0200

    [Feature] start implementing base image for ubuntu 16.04

--- cmake/use_cli11.cmake
+++ cmake/use_cli11.cmake
@@ -1,4 +1,5 @@
 set(CLI11_INSTALL OFF CACHE BOOL "CLI11 install overwritten")
 set(CLI11_SINGLE_FILE OFF CACHE BOOL "CLI11 single file overwritten")
+set(BUILD_TESTING OFF CACHE BOOL "Overwrite testing from cli11")
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/cli11")
-mark_as_advanced(FORCE CLI11_INSTALL CLI11_SINGLE_FILE)
+mark_as_advanced(FORCE CLI11_INSTALL CLI11_SINGLE_FILE BUILD_TESTING)
