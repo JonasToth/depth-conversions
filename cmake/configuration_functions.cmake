6e60504e1eb7f3848d05129fd33d023b7de21943
Jonas Toth <development@jonas-toth.eu>
2019-09-20 14:24:51 +0200

    [Feature] enable -Werror

--- cmake/configuration_functions.cmake
+++ cmake/configuration_functions.cmake
@@ -19,7 +19,7 @@ function(common_target_properties target_name)
             PUBLIC "GSL_TERMINATE_ON_CONTRACT_VIOLATION")
     endif (WITH_CONTRACT_EXCEPTION)
     target_compile_options(${target_name}
-        PRIVATE "-Wall" "-Wextra" "-mavx")
+        PRIVATE "-Wall" "-Wextra" "-Werror" "-mavx")
 
     if (CXX_COMPILER_ID EQUAL "GNU")
         target_compile_options(${target_name}
