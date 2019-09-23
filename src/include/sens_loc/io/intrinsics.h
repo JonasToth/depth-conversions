7dc06ce4748a70cf56649bdd88235f289322588d
Jonas Toth <development@jonas-toth.eu>
2019-09-23 16:59:56 +0200

    [Refactor] move io code into header

--- src/include/sens_loc/io/intrinsics.h
+++ src/include/sens_loc/io/intrinsics.h
@@ -4,6 +4,7 @@
 #include <istream>
 #include <optional>
 #include <sens_loc/camera_models/pinhole.h>
+#include <sstream>
 
 namespace sens_loc { namespace io {
 
@@ -17,8 +18,57 @@ namespace sens_loc { namespace io {
 /// 0.0  0.0  1.0
 /// ```
 /// @note values in brackets are optional.
-std::optional<camera_models::pinhole> load_pinhole_intrinsic(std::istream &in);
+inline std::optional<camera_models::pinhole>
+load_pinhole_intrinsic(std::istream &in) noexcept {
+    if (!in.good())
+        return std::nullopt;
 
+    camera_models::pinhole p;
+    double                 trash = 0.0;
+
+    std::string line;
+    {
+        std::getline(in, line);
+        std::istringstream ss{line};
+        // Parsing line 1 with expected format 'w h'.
+        ss >> p.w >> p.h;
+        Expects(p.w > 0);
+        Expects(p.h > 0);
+        if (ss.rdstate() != std::ios_base::eofbit)
+            return std::nullopt;
+    }
+
+    {
+        std::getline(in, line);
+        std::istringstream ss{line};
+        // Parsing line 2 with expected format 'fx 0.0 cx'.
+        ss >> p.fx >> trash >> p.cx;
+        Expects(p.fx > 0.);
+        Expects(p.cx > 0.);
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+        Expects(p.cx / static_cast<double>(p.w) < 0.75);
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+        Expects(p.cx / static_cast<double>(p.w) > 0.25);
+        if (ss.rdstate() != std::ios_base::eofbit)
+            return std::nullopt;
+    }
+
+    {
+        std::getline(in, line);
+        std::istringstream ss{line};
+        // Parsing line 3 with expected format '0.0 fy cy'.
+        ss >> trash >> p.fy >> p.cy;
+        Expects(p.fy > 0.);
+        Expects(p.cy > 0.);
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+        Expects(p.cy / static_cast<double>(p.h) < 0.75);
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+        Expects(p.cy / static_cast<double>(p.h) > 0.25);
+        if (ss.rdstate() != std::ios_base::eofbit)
+            return std::nullopt;
+    }
+    return p;
+}
 }}  // namespace sens_loc::io
 
 #endif /* end of include guard: INTRINSICS_H_6SQKKYBV */
