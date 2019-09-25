068a586616eed9e1e2229b3cfe7f5e738fe13e2c
Jonas Toth <kontakt@jonas-toth.eu>
2019-09-25 15:27:34 +0200

    [Fix] properly convert curvature images before writing it to disk in conversion-tool

--- src/apps/depth2x/converter_curvature.cpp
+++ src/apps/depth2x/converter_curvature.cpp
@@ -14,9 +14,9 @@ bool gauss_curv_converter::process_file(cv::Mat depth_image, int idx) const
 
     const cv::Mat gauss =
         depth_to_gaussian_curvature<double, double>(depth_image, intrinsic);
-    cv::Mat gauss_conv;
-    gauss.convertTo(gauss_conv, CV_16S);
-    bool success = cv::imwrite(fmt::format(_files.output, idx), gauss_conv);
+    const auto converted = conversion::curvature_to_image<double, ushort>(
+        gauss, depth_image, -10., 10.);
+    bool success = cv::imwrite(fmt::format(_files.output, idx), converted);
 
     return success;
 }
@@ -29,9 +29,9 @@ bool mean_curv_converter::process_file(cv::Mat depth_image, int idx) const
 
     const cv::Mat mean =
         depth_to_mean_curvature<double, double>(depth_image, intrinsic);
-    cv::Mat mean_conv;
-    mean.convertTo(mean_conv, CV_16S);
-    bool success = cv::imwrite(fmt::format(_files.output, idx), mean_conv);
+    const auto converted = conversion::curvature_to_image<double, ushort>(
+        mean, depth_image, -20., 20.);
+    bool success = cv::imwrite(fmt::format(_files.output, idx), converted);
 
     return success;
 }
