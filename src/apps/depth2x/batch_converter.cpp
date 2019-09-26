f2401811b4243dcff2196d97d63990f29f509f76
Jonas Toth <development@jonas-toth.eu>
2019-09-26 13:48:37 +0200

    [Fix] don't terminate with a contract violation if non-16-bit grayscale image is loaded in driver

--- src/apps/depth2x/batch_converter.cpp
+++ src/apps/depth2x/batch_converter.cpp
@@ -21,7 +21,8 @@ bool batch_converter::process_index(int idx) const noexcept {
 
     if (!depth_image)
         return false;
-    Expects((*depth_image).type() == CV_16U);
+    if (!(*depth_image).type() == CV_16U)
+        return false;
 
     cv::Mat pp_image = this->preprocess_depth(std::move(*depth_image));
     return this->process_file(std::move(pp_image), idx);
