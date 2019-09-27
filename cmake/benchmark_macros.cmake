587bceee0d33bff9508f2089f24a10698bdbf8e1
Jonas Toth <development@jonas-toth.eu>
2019-09-27 10:56:00 +0200

    [Feature] enable benchmark reports for some builders, remove unnecessary advanced builds

--- cmake/benchmark_macros.cmake
+++ cmake/benchmark_macros.cmake
@@ -3,5 +3,11 @@ macro(create_bm name source_file)
     target_link_libraries(bm_${name}
                           PRIVATE nonius pthread sens_loc::sens_loc)
     common_target_properties(bm_${name})
-    add_test(bm_${name} bm_${name})
+    if (WITH_BENCHMARK_JUNIT_REPORT)
+        add_test(NAME bm_${name}
+                 COMMAND bm_${name} -r junit -o bm_${name}.xml)
+    else ()
+        add_test(NAME bm_${name}
+                 COMMAND bm_${name})
+    endif ()
 endmacro()
