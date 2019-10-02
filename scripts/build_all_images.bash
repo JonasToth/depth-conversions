0dedc220f4f91f6d5359868f5f69a8611cf22d30
Jonas Toth <development@jonas-toth.eu>
2019-10-02 15:21:16 +0200

    [Feature] base images on container registry and rebuild them on docker-changes

--- scripts/build_all_images.bash
+++ scripts/build_all_images.bash
@@ -13,6 +13,14 @@ SCRIPT_PATH=$(dirname "${SCRIPT_FILE}")
 # shellcheck source=log_helpers.sh
 . "${SCRIPT_PATH}/log_helpers.sh"
 
+if [ $# -ge 1 ]; then
+    print_info "Pushing images to docker registry"
+    PUSH_REGISTRY=1
+else
+    print_info "NOT Pushing images to docker registry"
+    PUSH_REGISTRY=0
+fi
+
 print_info "Changing into docker directory"
 cd "${SCRIPT_PATH}/../docker/" || (print_error "Could not change into docker/ directory of this repository!"; exit 1)
 
@@ -27,6 +35,13 @@ for dir in */; do
         print_warning "Failed to build ${img_name}"
         failure=1
     fi
+
+    if [ ${PUSH_REGISTRY} -eq 1 ]; then
+        reg_name="git.informatik.tu-freiberg.de:5050/jtoth/master-thesis/${img_name}"
+        docker tag "${img_name}" "${reg_name}" || (print_warning "Can not tag ${img_name} for registry")
+        docker push "${reg_name}"
+    fi
+
     # shellcheck disable=SC2103
     cd ..
 done
