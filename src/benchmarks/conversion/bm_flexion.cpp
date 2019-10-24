07f12d904f6f0688eaab2984abb8f22502d70d2d
Jonas Toth <development@jonas-toth.eu>
2019-10-24 12:25:54 +0200

    [Benchmark] add flexion benchmark for laserscan image

--- src/benchmarks/conversion/bm_flexion.cpp
+++ src/benchmarks/conversion/bm_flexion.cpp
@@ -7,7 +7,6 @@
 using namespace sens_loc;
 using namespace conversion;
 
-
 NONIUS_BENCHMARK("Depth2Flexion", [](nonius::chronometer meter) {
     const auto [_, euclid, p] = get_data();
     (void) _;
@@ -32,3 +31,27 @@ NONIUS_BENCHMARK("Depth2Flexion Parallel", [](nonius::chronometer meter) {
         flow.clear();
     });
 })
+
+NONIUS_BENCHMARK("Depth2Flexion Laserscan", [](nonius::chronometer meter) {
+    const auto [euclid, p] = get_data_laserscan();
+    auto in                = euclid;
+    auto cali              = p;
+    meter.measure([&] { return depth_to_flexion(in, cali); });
+})
+
+NONIUS_BENCHMARK("Depth2Flexion Laserscan Parallel",
+                 [](nonius::chronometer meter) {
+                     const auto [euclid, p] = get_data_laserscan();
+
+                     auto         in   = euclid;
+                     auto         out  = euclid;
+                     auto         cali = p;
+                     tf::Executor exe;
+                     tf::Taskflow flow;
+
+                     meter.measure([&] {
+                         par_depth_to_flexion(in, cali, out, flow);
+                         exe.run(flow).wait();
+                         flow.clear();
+                     });
+                 })
--- src/benchmarks/conversion/util.h
+++ src/benchmarks/conversion/util.h
@@ -1,6 +1,9 @@
+#include <sens_loc/camera_models/equirectangular.h>
 #include <sens_loc/camera_models/pinhole.h>
 #include <sens_loc/conversion/depth_to_laserscan.h>
 #include <sens_loc/io/image.h>
+#include <sens_loc/math/angle_conversion.h>
+#include <sens_loc/math/image.h>
 #include <stdexcept>
 #include <tuple>
 
@@ -19,4 +22,25 @@ get_data() {
     const auto euclid = sens_loc::conversion::depth_to_laserscan(*img, p);
 
     return std::make_tuple(*img, euclid, p);
+}
+
+
+inline std::tuple<sens_loc::math::image<float>,
+                  sens_loc::camera_models::equirectangular<float>>
+get_data_laserscan() {
+    using namespace sens_loc;
+    constexpr camera_models::equirectangular<float> e_float{
+        /*width=*/1799,
+        /*height=*/397,
+        /*theta_range=*/
+        {math::deg_to_rad(50.f), math::deg_to_rad(130.f)}};
+
+    const std::optional<math::image<ushort>> img = io::load_image<ushort>(
+        "conversion/laserscan-depth-half.png", cv::IMREAD_UNCHANGED);
+    if (!img)
+        throw std::runtime_error{"No Data Found!"};
+
+    const auto euclid = math::convert<float>(*img);
+
+    return std::make_tuple(euclid, e_float);
 }
