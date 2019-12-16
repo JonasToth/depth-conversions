aa6d5f5a793f9fd5a7f0718f724df76ed607d76e
Jonas Toth <development@jonas-toth.eu>
2019-12-16 22:30:13 +0100

    [Refactor] remove code duplication for parallel batch process

--- src/apps/util/batch_converter.cpp
+++ src/apps/util/batch_converter.cpp
@@ -14,6 +14,7 @@
 #include <thread>
 
 namespace sens_loc::apps {
+
 bool batch_converter::process_index(int idx) const noexcept {
     Expects(!_files.input.empty());
 
@@ -40,60 +41,9 @@ batch_converter::preprocess_depth(const math::image<ushort>& depth_image) const
 }
 
 bool batch_converter::process_batch(int start, int end) const noexcept {
-    if (start > end)
-        std::swap(start, end);
-
-    bool batch_success = true;
-    try {
-        tf::Executor executor;
-        tf::Taskflow tf;
-        std::mutex   cout_mutex;
-        int          fails = 0;
-
-        tf.parallel_for(start, end + 1, 1,
-                        [&cout_mutex, &batch_success, &fails, this](int idx) {
-                            const bool success = this->process_index(idx);
-                            if (!success) {
-                                std::lock_guard l(cout_mutex);
-                                fails++;
-                                std::cerr << util::err{};
-                                std::cerr << "Could not process index \""
-                                          << rang::style::bold << idx << "\""
-                                          << rang::style::reset << "!\n";
-                                batch_success = false;
-                            }
-                        });
-
-        const auto before = std::chrono::steady_clock::now();
-        executor.run(tf).wait();
-        const auto after = std::chrono::steady_clock::now();
-
-        Ensures(fails >= 0);
-
-        std::cerr << util::info{};
-        std::cerr << "Processing " << rang::style::bold
-                  << std::abs(end - start) + 1 - fails << rang::style::reset
-                  << " images took " << rang::style::bold
-                  << std::chrono::duration_cast<std::chrono::seconds>(after -
-                                                                      before)
-                         .count()
-                  << "" << rang::style::reset << " seconds!\n";
-
-        if (fails > 0)
-            std::cerr << util::warn{} << "Encountered " << rang::style::bold
-                      << fails << rang::style::reset << " problematic files!\n";
-    } catch (const std::exception& e) {
-        std::cerr << util::err{} << "Problem occured during batch-processing!\n"
-                  << "Message: " << e.what() << "\n";
-        batch_success = false;
-    } catch (...) {
-        std::cerr << util::err{}
-                  << "Fatal problem occured while batch-processing!\n"
-                  << "Potential exhaustion of resources or an other system "
-                     "problem!\n";
-        batch_success = false;
-    }
-
-    return batch_success;
+    return parallel_indexed_file_processing(
+        start, end,
+        [this](int idx) -> bool { return this->process_index(idx); });
 }
+
 }  // namespace sens_loc::apps
--- src/apps/util/batch_converter.h
+++ src/apps/util/batch_converter.h
@@ -16,6 +16,74 @@ namespace sens_loc {
 /// but does not provide library functionality.
 namespace apps {
 
+/// Helper function that processes a range of files based on index.
+/// The boolean function \c f is applied to each function. Error handling
+/// and reporting is done if \c f returns \c false.
+///
+/// \tparam BoolFunction Apply this functor for each index.
+/// \param start,end inclusive range of integers for the files
+/// \param f functor that is applied for each index
+template <typename BoolFunction>
+bool parallel_indexed_file_processing(int          start,
+                                      int          end,
+                                      BoolFunction f) noexcept {
+    if (start > end)
+        std::swap(start, end);
+
+    bool batch_success = true;
+    try {
+        tf::Executor executor;
+        tf::Taskflow tf;
+        std::mutex   cout_mutex;
+        int          fails = 0;
+
+        tf.parallel_for(start, end + 1, 1,
+                        [&cout_mutex, &batch_success, &fails, &f](int idx) {
+                            const bool success = f(idx);
+                            if (!success) {
+                                std::lock_guard l(cout_mutex);
+                                fails++;
+                                std::cerr << util::err{};
+                                std::cerr << "Could not process index \""
+                                          << rang::style::bold << idx << "\""
+                                          << rang::style::reset << "!\n";
+                                batch_success = false;
+                            }
+                        });
+
+        const auto before = std::chrono::steady_clock::now();
+        executor.run(tf).wait();
+        const auto after = std::chrono::steady_clock::now();
+
+        Ensures(fails >= 0);
+
+        std::cerr << util::info{};
+        std::cerr << "Processing " << rang::style::bold
+                  << std::abs(end - start) + 1 - fails << rang::style::reset
+                  << " images took " << rang::style::bold
+                  << std::chrono::duration_cast<std::chrono::seconds>(after -
+                                                                      before)
+                         .count()
+                  << "" << rang::style::reset << " seconds!\n";
+
+        if (fails > 0)
+            std::cerr << util::warn{} << "Encountered " << rang::style::bold
+                      << fails << rang::style::reset << " problematic files!\n";
+    } catch (const std::exception& e) {
+        std::cerr << util::err{} << "Problem occured during batch-processing!\n"
+                  << "Message: " << e.what() << "\n";
+        batch_success = false;
+    } catch (...) {
+        std::cerr << util::err{}
+                  << "Fatal problem occured while batch-processing!\n"
+                  << "Potential exhaustion of resources or an other system "
+                     "problem!\n";
+        batch_success = false;
+    }
+
+    return batch_success;
+}
+
 /// \addtogroup conversion-driver
 /// @{
 
