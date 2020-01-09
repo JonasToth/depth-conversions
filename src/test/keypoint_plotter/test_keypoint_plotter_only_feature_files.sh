cd6e9238281505a70e2e9fbf5d73f350e96bdde3
Jonas Toth <development@jonas-toth.eu>
2020-01-09 14:13:04 +0100

    [Refactor] move keypoint plotter to .feature as well

--- src/test/keypoint_plotter/test_keypoint_plotter_only_feature_files.sh
+++ src/test/keypoint_plotter/test_keypoint_plotter_only_feature_files.sh
@@ -13,7 +13,7 @@ helpers="$2"
 print_info "Using \"${exe}\" as driver executable"
 
 if ! ${exe} \
-    --input sift-{}.sift \
+    --input sift-{}.feature \
     --output keypoints-{}.png \
     --start 0 --end 1 ; then
     print_error "Could not plot keypoint in selfincluded feature-file (8bit gray, 16bit gray)"
@@ -26,7 +26,7 @@ if [ ! -f keypoints-0.png ] || \
 fi
 
 if ! ${exe} \
-    --input sift-{}.sift \
+    --input sift-{}.feature \
     --output keypoints-blue-{}.png \
     --color blue \
     --start 0 --end 1 ; then
