e99a7f837b5fc3c97304eb6016824ed5a4d899d5
Jonas Toth <development@jonas-toth.eu>
2020-01-20 16:33:09 +0100

    [Misc] print vMAJOR.MINOR.PATCH

--- src/apps/util/version_printer.cpp
+++ src/apps/util/version_printer.cpp
@@ -7,7 +7,7 @@
 namespace sens_loc::apps {
 
 [[noreturn]] void print_version::operator()(int /*count*/) noexcept {
-    std::cout << program_name << " " << get_version() << "\n";
+    std::cout << program_name << " v" << get_version() << "\n";
     std::exit(EXIT_SUCCESS);
 }
 
