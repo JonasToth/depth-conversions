7729fa9e8c613af0c813de879ad96125f196b390
Jonas Toth <development@jonas-toth.eu>
2020-01-20 14:30:49 +0100

    [Feature] search libfmt from the system first and build it for the system in the msan image
    Signed-off-by: Jonas Toth <development@jonas-toth.eu>

--- cmake/use_fmtlib.cmake
+++ cmake/use_fmtlib.cmake
@@ -1,19 +1,19 @@
-set(FMT_DOC OFF CACHE BOOL "FMT Doc overwritten")
-set(FMT_FUZZ OFF CACHE BOOL "FMT Fuzz overwritten")
-set(FMT_INSTALL OFF CACHE BOOL "FMT Install overwritten")
-set(FMT_PEDANTIC ON CACHE BOOL "FMT Pedantic overwritten")
-set(FMT_TEST OFF CACHE BOOL "FMT Test overwritten")
-set(FMT_WERROR OFF CACHE BOOL "FMT WError overwritten")
+if (NOT FORCE_BUNDLED_FMT)
+    find_package(fmt QUIET)
+endif (NOT FORCE_BUNDLED_FMT)
 
-add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/fmt")
-mark_as_advanced(FORCE FMT_DOC FMT_FUZZ FMT_INSTALL FMT_PEDANTIC FMT_TEST
-                       FMT_WERROR)
+if (NOT TARGET fmt::fmt)
+    message(STATUS "Using bundled libfmt")
+    set(FMT_DOC OFF CACHE BOOL "FMT Doc overwritten")
+    set(FMT_FUZZ OFF CACHE BOOL "FMT Fuzz overwritten")
+    set(FMT_INSTALL OFF CACHE BOOL "FMT Install overwritten")
+    set(FMT_PEDANTIC ON CACHE BOOL "FMT Pedantic overwritten")
+    set(FMT_TEST OFF CACHE BOOL "FMT Test overwritten")
+    set(FMT_WERROR OFF CACHE BOOL "FMT WError overwritten")
 
-if (WITH_MSAN)
-    target_compile_options(fmt
-        PUBLIC
-        "-fsanitize=memory"
-        "-fsanitize-blacklist=${PROJECT_SOURCE_DIR}/scripts/msan_suppressions"
-        "-fsanitize-memory-track-origins"
-        "-fno-omit-frame-pointer")
-endif (WITH_MSAN)
+    add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/third_party/fmt")
+    mark_as_advanced(FORCE FMT_DOC FMT_FUZZ FMT_INSTALL FMT_PEDANTIC FMT_TEST
+                           FMT_WERROR)
+else ()
+    message(STATUS "Found libfmt in the system")
+endif (NOT TARGET fmt::fmt)
