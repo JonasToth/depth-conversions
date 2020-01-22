5f7aaa167a770489e1200bf34eb34eb99fc79afe
Jonas Toth <development@jonas-toth.eu>
2020-01-22 15:23:21 +0100

    [Feature] implement keypoint distance analysis

--- src/apps/feature_performance/keypoint_distribution.cpp
+++ src/apps/feature_performance/keypoint_distribution.cpp
@@ -9,7 +9,6 @@
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/variance.hpp>
 #include <boost/histogram.hpp>
-#include <boost/histogram/make_histogram.hpp>
 #include <boost/histogram/ostream.hpp>
 #include <fstream>
 #include <gsl/gsl>
@@ -44,23 +43,86 @@ std::string histogram_to_gnuplot(const Histogram2D& h) noexcept {
 /// Calculate the 2-dimensional distribution of the keypoints for a dataset.
 class keypoint_distribution {
   public:
-    keypoint_distribution(
-        gsl::not_null<std::mutex*>                m,
-        gsl::not_null<std::vector<cv::KeyPoint>*> points) noexcept
-        : process_mutex{m}
-        , global_keypoints{points} {}
+    keypoint_distribution(gsl::not_null<std::mutex*> points_mutex,
+                          gsl::not_null<std::vector<cv::KeyPoint>*> points,
+                          gsl::not_null<std::mutex*>         distance_mutex,
+                          gsl::not_null<std::vector<float>*> distances) noexcept
+        : keypoint_mutex{points_mutex}
+        , global_keypoints{points}
+        , distance_mutex{distance_mutex}
+        , global_distances{distances} {}
 
-    void operator()(int /*idx*/,
-                    std::optional<std::vector<cv::KeyPoint>> keypoints,
-                    std::optional<cv::Mat> /*descriptors*/) noexcept {
-        std::lock_guard guard{*process_mutex};
-        global_keypoints->insert(std::end(*global_keypoints),
-                                 std::begin(*keypoints), std::end(*keypoints));
+    void
+    operator()(int /*idx*/,
+               std::optional<std::vector<cv::KeyPoint>> keypoints,
+               std::optional<cv::Mat> /*descriptors*/) noexcept {  // NOLINT
+        if (keypoints->empty())
+            return;
+
+        // Simply insert the keypoints in the global keypoint vector.
+        {
+            std::lock_guard guard{*keypoint_mutex};
+            global_keypoints->insert(std::end(*global_keypoints),
+                                     std::begin(*keypoints),
+                                     std::end(*keypoints));
+        }
+
+        // Calculate the minimal distance of each keypoint to all others
+        // and insert that first into a local vector with that information
+        // and finally into the global vector with that information.
+        // This is the pixel-distance with euclidean norm.
+        // NOTE: This is an inefficient implementation if O(n^2) complexity.
+        {
+            const std::size_t n_points = keypoints->size();
+            if (n_points < 2)
+                return;
+
+            std::vector<cv::KeyPoint>& kp_ref = *keypoints;
+            std::vector<float>         local_minima;
+            std::vector<float>         local_distances;
+            local_distances.reserve(n_points);
+
+            // The upper triangle of the distance matrix needs to be calculated.
+            // The last row must be ignored and the diagonal element will be 0.
+            // Example:
+            //    ++p1++p2++p3++
+            // p1 |  0   2   4 |
+            // p2 |      0   7 |
+            // p3 |          0 |
+            // The result are 'n_points / 2' number of minimal distances.
+            // Because there will
+            for (std::size_t i = 1; i < n_points - 1; ++i) {
+                // loop-calculation
+                for (std::size_t k = i + 1; k < n_points; ++k) {
+                    const float dx = kp_ref[i].pt.x - kp_ref[k].pt.x;
+                    const float dy = kp_ref[i].pt.y - kp_ref[k].pt.y;
+                    const float d  = std::sqrt(dx * dx + dy * dy);
+                    local_distances.emplace_back(d);
+                }
+
+                Ensures(!local_distances.empty());
+
+                // Store the minimal element for statistical evaluation.
+                local_minima.emplace_back(*std::min_element(
+                    std::begin(local_distances), std::end(local_distances)));
+
+                // Ensure that local_distances is only allocated once.
+                local_distances.clear();
+            }
+
+            // Insertion
+            {
+                std::lock_guard guard{*distance_mutex};
+                global_distances->insert(global_distances->end(),
+                                         std::begin(local_minima),
+                                         std::end(local_minima));
+            }
+        }
     }
 
     auto postprocess(int image_width, int image_height) {
-        std::lock_guard guard{*process_mutex};
         Expects(!global_keypoints->empty());
+        Expects(!global_distances->empty());
 
         using namespace boost::histogram;
         // Number of bins in one direction for 2D histogram.
@@ -104,6 +166,19 @@ class keypoint_distribution {
                           size_histo(kp.size);
                       });
 
+
+        accumulator_set<float,
+                        stats<tag::count, tag::min, tag::max, tag::median,
+                              tag::mean, tag::variance(lazy)>>
+            dist_stat;
+        std::for_each(global_distances->begin(), global_distances->end(),
+                      [&](float d) { dist_stat(d); });
+        auto dist_bins  = 50;
+        auto dist_histo = make_histogram(
+            axis::regular(dist_bins, min(dist_stat) - 0.01F,
+                          max(dist_stat) + 0.01F, "minimal distance"));
+        dist_histo.fill(*global_distances);
+
         std::cout << "==== Response\n"
                   << "count:  " << count(response_stat) << "\n"
                   << "min:    " << min(response_stat) << "\n"
@@ -117,7 +192,14 @@ class keypoint_distribution {
                   << "max:    " << max(size_stat) << "\n"
                   << "median: " << median(size_stat) << "\n"
                   << "mean:   " << mean(size_stat) << "\n"
-                  << size_histo << std::endl;
+                  << size_histo << "\n"
+                  << "=== Distance\n"
+                  << "count:  " << count(dist_stat) << "\n"
+                  << "min:    " << min(dist_stat) << "\n"
+                  << "max:    " << max(dist_stat) << "\n"
+                  << "median: " << median(dist_stat) << "\n"
+                  << "mean:   " << mean(dist_stat) << "\n"
+                  << dist_histo << "\n";
 
         std::ofstream gnuplot_data{"location_histo.data"};
         gnuplot_data << histogram_to_gnuplot(location_histo) << std::endl;
@@ -125,8 +207,11 @@ class keypoint_distribution {
 
   private:
     // Data required for the parallel processing.
-    gsl::not_null<std::mutex*>                process_mutex;
+    gsl::not_null<std::mutex*>                keypoint_mutex;
     gsl::not_null<std::vector<cv::KeyPoint>*> global_keypoints;
+
+    gsl::not_null<std::mutex*>         distance_mutex;
+    gsl::not_null<std::vector<float>*> global_distances;
 };
 
 }  // namespace
@@ -140,13 +225,18 @@ int analyze_keypoint_distribution(std::string_view input_pattern,
     using visitor =
         statistic_visitor<keypoint_distribution, required_data::keypoints>;
 
-    std::mutex                process_mutex;
+    std::mutex                keypoint_mutex;
     std::vector<cv::KeyPoint> global_keypoints;
 
-    auto f = parallel_visitation(start_idx, end_idx,
-                                 visitor{input_pattern,
-                                         gsl::not_null{&process_mutex},
-                                         gsl::not_null{&global_keypoints}});
+    std::mutex         distance_mutex;
+    std::vector<float> global_minimal_distances;
+
+    auto f = parallel_visitation(
+        start_idx, end_idx,
+        visitor{input_pattern, gsl::not_null{&keypoint_mutex},
+                gsl::not_null{&global_keypoints},
+                gsl::not_null{&distance_mutex},
+                gsl::not_null{&global_minimal_distances}});
 
     f.postprocess(image_width, image_height);
 
