c3cebfd4cff8a9807a9b3177edd73e72654b9c27
Jonas Toth <development@jonas-toth.eu>
2020-02-13 16:22:50 +0100

    [Feature] adjust histogram and distribution plotting to see range of data better
    Signed-off-by: Jonas Toth <development@jonas-toth.eu>

--- scripts/plot/plot_histogram.sh
+++ scripts/plot/plot_histogram.sh
@@ -13,8 +13,12 @@ output="$2"
 gnuplot <<EOF
 set terminal postscript enhanced eps color
 set output "$output"
-set yrange [200:0]
-set xrange [0:]
 
-plot "$input" using 1:2:3 with image notitle
+set xrange [-2:]
+set mytics
+set grid ytics mytics back ls 0, ls 0
+set style histogram rowstacked gap 0
+set style fill solid 0.2 border lt -1
+
+plot "$input" using 1:2 smooth freq with boxes notitle
 EOF
